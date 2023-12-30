3d854388d3a75932b596d4be62b5bc82765d7bdb
foy4748 <faisaljfcl@gmail.com>
2023-12-31 00:17:36 +0600

    Initial HTML, CSS setup

---
+++ assets/js/index.js
@@ -0,0 +1,28 @@
+/*
+const images = [
+  {
+    img: "https://m.media-amazon.com/images/I/71ctRE34RuL._AC_UY218_.jpg",
+    price: "361.55",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/71v0BQo8T8L._AC_UY218_.jpg",
+    price: "241.83",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/711r+iB8DZL._AC_UY218_.jpg",
+    price: "174.94",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/71jTFnk7XiL._AC_UY218_.jpg",
+    price: "307.71",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/81vnNsRDo6L._AC_UY218_.jpg",
+    price: "488.28",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/71ctRE34RuL._AC_UL320_.jpg",
+    price: "455.51",
+  },
+];
+*/
