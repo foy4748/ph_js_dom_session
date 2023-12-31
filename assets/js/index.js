e0a28cf85351c888d0ef328061c39ba47ec5cafb
foy4748 <faisaljfcl@gmail.com>
2023-12-31 18:28:02 +0600

    Implemented cart state management

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,11 +5,27 @@
 // U - Update || Data that can be read
 // D - Delete || Delete data that can be read
 
+const globalState = {};
+/*
+  {
+    "product Name 1": {
+      price: Number,
+      quantity: Number
+    },
+    "product Name 2": {
+      price: Number,
+      quantity: Number
+    },
+  }
+*/
+
+
 // Grabbing Elements
 const cards = document.querySelectorAll(".card");
 
 //console.log(cards[0].childNodes[3].childNodes[5].childNodes[1]);
 
+// Adding EventListeners to card
 cards.forEach((singleCard, idx) => {
   //console.log(singleCard.childNodes[3].childNodes[5].childNodes[1]);
   //console.log(singleCard.childNodes[3].childNodes[3].textContent.split(' ')[1])
@@ -17,17 +33,59 @@ cards.forEach((singleCard, idx) => {
   //console.log(article.childNodes[1].innerText);
 
   const article = singleCard.childNodes[3];
+
   const addToCartButton = article.childNodes[5].childNodes[1];
   const price = article.childNodes[3].textContent.split(' ')[1]
   const productName = article.childNodes[1].innerText
   
   //const price = addToCartButton.dataset.price;
 
+  const productImg = singleCard.childNodes[1].childNodes[1].src;
+
   addToCartButton.addEventListener('click', () => {
-    console.log(`Clicked ${idx} || Price ${price} || Product Name: ${productName}`)
+    //console.log(`Clicked ${idx} || Price ${price} || Product Name: ${productName}`)
+
+    // Updating Global State
+    if(productName in globalState){
+      globalState[productName]['quantity']++;
+    } else{
+      globalState[productName] = {};
+      globalState[productName]['price'] = price;
+      globalState[productName]['quantity'] = 1;
+    }
+    console.log(globalState)
   })
 
 })
+
+function createCartItem(img_url, product_title, product_price, product_quantity) {
+  const singleCartItemTemplate = `
+            <section class="cart-item">
+              <section class="flex flex-between pe-1">
+                <figure class="cart-figure">
+                  <img class="cart-img" src="${img_url}" alt="${product_title}">
+                  <article>
+                    <h3 class="pb-0 mb-0">${product_title}</h3>
+                  </article>
+                </figure>
+                <article class="flex w-50 flex-between">
+                  <div>
+                    <p>Unit Price</p>
+                    <p><strong>${product_price}</strong></p>
+                  </div>
+                  <div>
+                    <p>Quantity</p>
+                    <p><strong>${product_quantity}</strong></p>
+                  </div>
+                </article>
+              </section>
+              <article class="text-center cart-total-btn">
+                <p class="p-0 m-0"><strong>Total: ${(product_price*product_quantity).toFixed(2)} BDT</strong></p>
+              </article>
+            </section>
+  `
+  return singleCartItemTemplate;
+}
 
 /*
 const images = [
